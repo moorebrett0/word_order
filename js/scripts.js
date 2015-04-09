f6ade7d4e9abdaf23021288aad4d51052c934aa6
Brett Moore & Kyle Bulloch <student@epicodus.com>
2015-04-09 09:50:43 -0700

    add functionality to our second test cleverly using arrays with a key

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 var scrabbleScore = function(input) {
-    return 1;
+    var letters = {'a':1,'e':1,'i':1,'o':1,'l':1,'n':1,'r':1,'s':1,'t':1,
+                    'd':2, 'g':2};
+    console.log(letters);
+    var score = 0;
+    var split_input = input.split("");
+    console.log(split_input);
+
+    for (var i = 0; i < split_input.length; i++) {
+        score += letters[split_input[i]];
+    }
+
+    return score;
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,15 @@
 describe('scrabbleScore', function() {
+
     it("will return a score of one if the user inputs a", function() {
         expect(scrabbleScore('a')).to.equal(1);
     });
+
     it("will return a score of 2 if the user inputs as", function(){
         expect(scrabbleScore('as')).to.equal(2);
     });
+
+    it("will return a score of 5 if the user inputs dog", function(){
+        expect(scrabbleScore('dog')).to.equal(5);
+    });
+
 });
